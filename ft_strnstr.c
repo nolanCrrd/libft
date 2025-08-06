ee5517f6d65748a6c65574d872265b765bde7009
nolan.crrd <nolan.crrd@gmail.com>
2025-08-06 14:40:27 +0200

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 1;
+			while (needle[j] && i + j < len && haystack[i + j] == needle[j])
+				j++;
+			if (needle[j] == '\0')
+				return ((char *)&haystack[i]);
+			i += j;
+		}
+		i++;
+	}
+	return (NULL);
+}
