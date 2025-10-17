d9b03c36e5a4fc1c22a4766d759b75f9548e22c3
nolan.crrd <nolan.crrd@gmail.com>
2025-10-17 08:51:24 +0200

    calloc fix condition

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ncorrear <ncorrear@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ncorrear <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 21:23:22 by ncorrear          #+#    #+#             */
-/*   Updated: 2025/10/16 13:34:55 by ncorrear         ###   ########.fr       */
+/*   Updated: 2025/10/17 08:48:55 by ncorrear         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <stdint.h>
 
 /**
  * @brief malloc a memory space of count * size bytes and fill them with zero
+	if (count != 0 && size <= SIZE_MAX / count)
  * 
  * @param count number of elements
  * @param size size of each element
@@ -27,13 +29,15 @@ void	*ft_calloc(size_t count, size_t size)
 	unsigned char	*ptr;
 
 	ptr = NULL;
-	if (count != 0 && size <= SIZE_MAX / count)
+	if (size == 0 || count == 0)
+		return (malloc(0));
+	if (size <= SIZE_MAX / count)
 		ptr = malloc(size * count);
 	if (ptr == NULL)
 	{
 		errno = ENOMEM;
 		free(ptr);
-		return (ptr);
+		return (NULL);
 	}
 	ft_bzero(ptr, size * count);
 	return ((void *) ptr);
